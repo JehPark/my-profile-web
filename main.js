5059eb62e6e3b8e3988100af3c8727b329dfd67c
JehPark <qkrwpgjs1230@naver.com>
2021-07-28 17:51:34 +0900

    final touch

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 const navbarName = document.querySelector("#navbar__logo__name");
 const activeScroll = document.querySelectorAll(".navbar__menu__item");
-
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
@@ -14,17 +14,21 @@ document.addEventListener('scroll', () => {
         activeScroll.forEach((item) =>{
             item.classList.add('scroll');
         })
+        navbarToggleBtn.classList.add('scroll--white');
     }else{
         navbar.classList.remove('navbar--dark');
         navbarName.classList.remove('log--white');
         activeScroll.forEach((item) =>{
             item.classList.remove('scroll');
         })
+        navbarToggleBtn.classList.remove('scroll--white');
     }
 })
 
-// Handle scrolling when tapping on the menu
 
+
+
+// Handle scrolling when tapping on the menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
     const target = event.target;
@@ -32,7 +36,15 @@ navbarMenu.addEventListener('click', (event)=>{
     if (link == null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+})
+
+// Navbar toggle button for small screen
+
+navbarToggleBtn.addEventListener('click', ()=>{
+    console.log('ok');
+    navbarMenu.classList.toggle('open');
 })
 
 // Handle click on "contact me" button on home
@@ -41,6 +53,7 @@ const homeContact = document.querySelector('.home__contact');
 homeContact.addEventListener('click', (event)=>{
     const link = event.target.dataset.link;
     scrollIntoView(link);
+    
 })
 
 // Make home slowly fade to transparent as the window scrolls down
@@ -96,8 +109,55 @@ workBtnContainer.addEventListener('click', (e) => {
     }, 300);
 })
 
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:'smooth'})
+
+const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonials', '#contact'];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+const observerOptions ={
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2,
 }
 
+let selectedNavItem = navItems[0];
+let selectedNavIndex = 0;
+
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
+
+
+const observerCallback = (entries, observer)=>{
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            if(entry.boundingClientRect.y < 0){
+                selectedNavIndex = index + 1;
+            }else{
+                selectedNavIndex = index - 1;
+            }
+        }
+
+    })
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', ()=>{
+    if (window.scrollY === 0){
+        selectedNavIndex = 0;
+    }else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight){
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
